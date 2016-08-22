c72ea41ce1901dab155e67c169647ee0683fa10a
Geert Braekmans <geert.braekmans@gmail.com>
2016-08-22 23:52:07 +0000

    Fixed the rename transaction to task

--- src/core/main.sh
+++ src/core/main.sh
@@ -2,6 +2,7 @@ include core/error
 include core/version
 include core/help
 include core/msg
+include core/task
 
 __main_is_query_mode() {
 	## Checks wether the given arguments put enSHure in query or execution mode.
@@ -9,26 +10,35 @@ __main_is_query_mode() {
 	[ "$(echo "$1" | head -c1)" = "-" ]
 }
 
-__main_query_transaction() {
+__main_query_task() {
 	## Gets called if enshure -t is given.
 	##$1 one of "begin" or "end"
+	##$2 if task is begin, the name of the task
 
 	# Check if argument is there
 	if [ -z "${1:-}" ]; then
-		msg_error "No transaction type specified. Add 'begin' or 'end'."
+		error "No task specified. Add 'begin' or 'end'."
 		exit "$_E_ARGUMENT_MISSING"
 	fi
 
 	# Parse the argument
 	case "$1" in
 		"begin")
-			__msg_begin
+			if [ -z "${2:-}" ]; then
+				error "No name for the task specified."
+				exit "$_E_ARGUMENT_MISSING"
+			fi
+			task_begin "$2"
 			;;
 		"end")
-			__msg_end
+			if [ -n "${2:-}" ]; then
+				error "A name for an ending task is given. This is not supported."
+				exit "$_E_ARGUMENT_MISSING"
+			fi
+			task_end
 			;;
 		*)
-			msg_error "Transaction type '$1' is invalid: must be 'begin' or 'end'."
+			error "--task '$1' is invalid: must be 'begin' or 'end'."
 			exit "$_E_INVALID_ENUM"
 			;;
 	esac
@@ -44,9 +54,9 @@ __main_query_mode_parse() {
 		"-v"|"--version")
 			printf "%s\n" "$_VERSION"
 			;;
-		"-t"|"--transaction")
+		"-t"|"--task")
 			shift;
-			__main_query_transaction "$@"
+			__main_query_task "$@"
 	esac
 }
 
---
+++ src/core/task.sh
@@ -0,0 +1,9 @@
+include core/msg
+
+task_begin() {
+	__msg HEADING "Task: $1"
+}
+
+task_end() {
+	__msg HEADING "Task done"
+}
