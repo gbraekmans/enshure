5c7d8965f08e50be0a29ff64f48b48d18c765df6
Geert Braekmans <geert.braekmans@gmail.com>
2016-10-01 11:01:05 +0200

    Fixed tests

--- test/modules/mysql_database.sh
+++ test/modules/mysql_database.sh
@@ -4,25 +4,21 @@ test_mysql_database() {
 
 	RESULT=$(enshure mysql_database tst present)
 	assertTrue 1 "$?"
-	assertEquals 2 "" "$RESULT"
+	assertEquals 2 "CHANGE: Mysql database tst is now present." "$RESULT"
 
 	stub mysql mysql_database_present
 
 	RESULT=$(enshure mysql_database tst present)
 	assertTrue 3 "$?"
-	assertEquals 4 "" "$RESULT"
+	assertEquals 4 "OK: Mysql database tst is already present." "$RESULT"
 
 	RESULT=$(enshure mysql_database tst absent)
 	assertTrue 5 "$?"
-	assertEquals 6 "" "$RESULT"
+	assertEquals 6 "CHANGE: Mysql database tst is now absent." "$RESULT"
 
 	stub mysql mysql_database_absent
 
 	RESULT=$(enshure mysql_database tst absent)
 	assertTrue 7 "$?"
-	assertEquals 8 "" "$RESULT"
-
-	enshure -q command_output
-
-	cat "$ENSHURE_LOG"
+	assertEquals 8 "OK: Mysql database tst is already absent." "$RESULT"
 }
