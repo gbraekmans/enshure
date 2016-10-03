d99c866ef3a5c9943f41c3c36562179f84863641
Geert Braekmans <geert.braekmans@gmail.com>
2016-10-03 10:22:22 +0200

    Added a password mysql_user

--- src/modules/mysql_user.sh
+++ src/modules/mysql_user.sh
@@ -3,29 +3,45 @@ module_description "$(translate "Creates or removes MySQL users")"
 
 argument username string identifier "$(translate "The name of the user")" "wp"
 argument host string required "$(translate "The host of the user")" "localhost" "127.0.0.1"
+argument password string optional "$(translate "The password of the user")" "s3cr3t"
 argument login string required "$(translate "The name of the login user")" "wp" "root"
-
 
 mysql='mysql --skip-column-names  --batch'
 
 is_state_present() {
 	# Check if user is present
-	query="SELECT 1 FROM dual WHERE EXISTS (SELECT * FROM mysql.user WHERE CONCAT(user, '@', host) = '${username}@${host}');"
-	[ -n "$($mysql --user="$login" --execute="$query")" ]
+	query="SELECT 1 FROM mysql.user WHERE user = '${username}' AND  host = '${host}'"
+	# if a password is set, add it as an extra check
+	if [ -n "$password" ]; then
+		query="$query AND password = PASSWORD('$password')"
+	fi
+	query="$query LIMIT 1;"
+
+	if [ -z "$($mysql --user="$login" --execute="$query")" ]; then
+		return 1
+	fi
+	return 0
  }
 
 is_state_absent() {
-	! is_state_present
+	query="SELECT 1 FROM mysql.user WHERE user = '${username}' AND  host = '${host}' LIMIT 1;"
+	[ -z "$($mysql --user="$login" --execute="$query")" ]
 }
 
 attain_state_present() {
-	# If not exists just to be safe
-	query="CREATE USER '${username}'@'${host}';"
-	run "$mysql --user='$login' --execute='$query'"
+	query="SELECT 1 FROM mysql.user WHERE user = '${username}' AND  host = '${host}' LIMIT 1;"
+	if [ -z "$($mysql --user="$login" --execute="$query")" ]; then
+		query="CREATE USER '${username}'@'${host}';"
+		run "$mysql --user=\"$login\" --execute=\"$query\""
+	fi
+
+	if [ -n "$password" ]; then
+		query="SET PASSWORD FOR '$username'@'$host' = PASSWORD('$password');"
+		run "$mysql --user=\"$login\" --execute=\"$query\""
+	fi
 }
 
 attain_state_absent() {
-	# If exists just to be safe
 	query="DROP USER '${username}'@'${host}';"
-	run "$mysql --user='$login' --execute='$query'"
+	run "$mysql --user=\"$login\" --execute=\"$query\""
 }
--- test/modules/mysql_user.sh
+++ test/modules/mysql_user.sh
@@ -21,4 +21,15 @@ test_mysql_user() {
 	assertTrue 7 "$?"
 	assertEquals 8 "OK: Mysql user tst is already absent." "$RESULT"
 
+	stub mysql mysql_user_absent_pw
+
+	RESULT=$(enshure mysql_user tst present host localhost password tst)
+	assertTrue 9 "$?"
+	assertEquals 10 "CHANGE: Mysql user tst is now present." "$RESULT"
+
+	stub mysql mysql_user_present_pw
+
+	RESULT=$(enshure mysql_user tst present host localhost password tst)
+	assertTrue 11 "$?"
+	assertEquals 12 "OK: Mysql user tst is already present." "$RESULT"
 }
