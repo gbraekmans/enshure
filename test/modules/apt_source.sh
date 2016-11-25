6120f3d1e8ad76258e96c99cb84673e02a05266e
Geert Braekmans <geert.braekmans@gmail.com>
2016-11-25 15:44:14 +0100

    Don't leave temporary files /tmp

--- test/modules/apt_source.sh
+++ test/modules/apt_source.sh
@@ -17,4 +17,6 @@ test_apt_source() {
 	RESULT=$(enshure apt_source "http://ftp.nluug.nl/pub/os/Linux/distr/debian/" absent distro stretch components main)
 	assertTrue 7 "$?"
 	assertEquals 8 "OK: Apt source http://ftp.nluug.nl/pub/os/Linux/distr/debian/ is already absent." "$RESULT"
+
+	rm -rf "$_apt_sources_list_location"
 }
